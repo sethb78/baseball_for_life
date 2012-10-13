3ee4e461b643dc9e7fbd4f3fedaa92678b3800e2
Seth B. Altshuler <sethb78@gmail.com>
2012-10-13 18:23:32 -0400

    Finish user edit, update, index, and destroy actions

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -10,40 +10,55 @@
 #
 
 require 'spec_helper'
+
 describe User do
 
-
   before do
-    @user = User.new(name: "Example User", email: "user@example.com", password: "foobar", password_confirmation: "foobar")
+    @user = User.new(name: "Example User", email: "user@example.com", 
+                     password: "foobar", password_confirmation: "foobar")
   end
 
   subject { @user }
 
   it { should respond_to(:name) }
   it { should respond_to(:email) }
-  it { should respond_to(:password_digest ) }
-  it { should respond_to(:password ) }
-  it { should respond_to(:password_confirmation ) }
-  it { should respond_to(:remember_token) }
-  it {should respond_to(:authenticate ) }
+  it { should respond_to(:password_digest) }
+  it { should respond_to(:password) }
+  it { should respond_to(:password_confirmation) }
+    it { should respond_to(:remember_token) }
+    it { should respond_to(:authenticate) }
+  it { should respond_to(:admin) }
+  it { should respond_to(:authenticate) }
 
   it { should be_valid }
+  it { should_not be_admin }
+
+  describe "with admin attribute set to 'true'" do
+    before do
+      @user.save!
+      @user.toggle!(:admin)
+    end
+
+    it { should be_admin }
+  end
 
   describe "when name is not present" do
     before { @user.name = " " }
     it { should_not be_valid }
   end
 
-  describe "when email is not present" do
-  	before { @user.email = " "}
-  	it { should_not be_valid}
+describe "when email is not present" do
+    before { @user.email = " " }
+    it { should_not be_valid }
   end
 
-  describe "when name is too long" do
-  	before {@user.name ="a" *51}
-  	it {should_not be_valid}
+   describe "when name is too long" do
+    before { @user.name = "a" * 51 }
+    it { should_not be_valid }
   end
-  describe "when email format is invalid" do
+
+
+describe "when email format is invalid" do
     it "should be invalid" do
       addresses = %w[user@foo,com user_at_foo.org example.user@foo.
                      foo@bar_baz.com foo@bar+baz.com]
@@ -64,28 +79,29 @@ describe User do
     end
   end
 
-  describe "when email address is already taken" do
-     before do
+ describe "when email address is already taken" do
+    before do
       user_with_same_email = @user.dup
       user_with_same_email.email = @user.email.upcase
       user_with_same_email.save
     end
+
+    it { should_not be_valid }
+  end
+describe "when password is not present" do
+    before { @user.password = @user.password_confirmation = " " }
     it { should_not be_valid }
   end
 
-describe "when password is not present" do
-  before { @user.password = @user.password_confirmation = " " }
-  it { should_not be_valid }
-end
+  describe "when password doesn't match confirmation" do
+    before { @user.password_confirmation = "mismatch" }
+    it { should_not be_valid }
+  end
 
-describe "when password doesn't match confirmation" do
-  before { @user.password_confirmation = "mismatch" }
-  it { should_not be_valid }
-end
-describe "when password confirmation is nil" do
-  before { @user.password_confirmation = nil }
-  it { should_not be_valid }
-end
+  describe "when password confirmation is nil" do
+    before { @user.password_confirmation = nil }
+    it { should_not be_valid }
+  end
 
 
 describe "with a password that's too short" do
@@ -108,7 +124,7 @@ describe "with a password that's too short" do
       specify { user_for_invalid_password.should be_false }
     end
   end
-  describe "remember token" do
+ describe "remember token" do
     before { @user.save }
     its(:remember_token) { should_not be_blank }
   end
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -4,20 +4,47 @@ describe "User pages" do
 
 	subject { page }
 
-	  describe "profile page" do
-	    let(:user) { FactoryGirl.create(:user) }
-	    before { visit user_path(user) }
+	describe "index" do
 
-	    it { should have_selector('h1',    text: user.name) }
-	    it { should have_selector('title', text: user.name) }
-	  end
+    		let(:user) { FactoryGirl.create(:user) }
 
-	  describe "signup page" do
-	    before { visit signup_path }
+    		before(:each) do
+      			sign_in user
+      			visit users_path
+    		end
 
-	    it { should have_selector('h1',    text: 'Sign up') }
-	    it { should have_selector('title', text: 'Sign up') }
-	  end
+    		it { should have_selector('title', text: 'All users') }
+    		it { should have_selector('h1',    text: 'All users') }
+
+    		describe "pagination" do
+
+      			before(:all) { 30.times { FactoryGirl.create(:user) } }
+      			after(:all)  { User.delete_all }
+
+      			it { should have_selector('div.pagination') }
+
+      			it "should list each user" do
+        			User.paginate(page: 1).each do |user|
+          				page.should have_selector('li', text: user.name)
+        			end
+      			end
+    		end
+  	end
+
+  	describe "profile page" do
+	    	let(:user) { FactoryGirl.create(:user) }
+	    	before { visit user_path(user) }
+
+	    	it { should have_selector('h1',    text: user.name) }
+	    	it { should have_selector('title', text: user.name) }
+	end
+
+	describe "signup page" do
+	    	before { visit signup_path }
+
+	    	it { should have_selector('h1',    text: 'Sign up') }
+	    	it { should have_selector('title', text: 'Sign up') }
+	end
 
 	describe "signup" do
 		before { visit signup_path }
@@ -30,23 +57,46 @@ describe "User pages" do
 			end
 		end
 
-		
-    describe "with valid information" do
+		describe "with valid information" do
 
-      before {signup}
+      			before {signup}
 
-      describe "after saving the user" do
-        before { click_button submit }
-        let(:user) { User.find_by_email('user@example.com') }
+      			describe "after saving the user" do
+        			before { click_button submit }
+        			let(:user) { User.find_by_email('user@example.com') }
 
-        it { should have_selector('title', text: user.name) }
-        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
-        it { should have_link('Sign out') }
-      end
+        			it { should have_selector('title', text: user.name) }
+        			it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+        			it { should have_link('Sign out') }
+      			end
       
-      it "should create a user" do
-        expect { click_button submit }.to change(User, :count).by(1)
-      end
+      			it "should create a user" do
+        			expect { click_button submit }.to change(User, :count).by(1)
+      			end
 		end
 	end
+
+	describe "edit" do
+	    	let(:user) { FactoryGirl.create(:user) }
+	    	before do
+	      		sign_in user
+	      	visit edit_user_path(user)
+	    end
+
+    		let(:user) { FactoryGirl.create(:user) }
+    		before { visit edit_user_path(user) }
+
+    		describe "page" do
+		      	it { should have_selector('h1',    text: "Update your profile") }
+		      	it { should have_selector('title', text: "Edit user") }
+		      	it { should have_link('change', href: 'http://gravatar.com/emails') }
+    		end
+
+    		describe "with invalid information" do
+      			before { click_button "Save changes" }
+
+      			it { should have_content('error') }
+    		end
+  	end
 end
+
