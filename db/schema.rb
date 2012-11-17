c820318000cb98d60d102c1b23a17c5b6e184195
Seth B. Altshuler <sethb78@gmail.com>
2012-11-17 18:40:46 -0500

    finish user edit, update, index, and destroy actions

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121110190737) do
+ActiveRecord::Schema.define(:version => 20121115224702) do
 
   create_table "history_teams", :force => true do |t|
     t.string   "Year"
@@ -88,6 +88,13 @@ ActiveRecord::Schema.define(:version => 20121110190737) do
     t.text     "Dud"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+  end
+
+  create_table "relationships", :force => true do |t|
+    t.integer  "follower_id"
+    t.integer  "followed_id"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
   end
 
   create_table "users", :force => true do |t|
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,35 +2,46 @@ require 'spec_helper'
 
 describe "Site Pages" do
 
-  subject { page }
+  	subject { page }
 
-  shared_examples_for "all static pages" do
-    it { should have_selector('h1',    text: heading) }
-    it { should have_selector('title', text: full_title(page_title)) }
-  end
+  	shared_examples_for "all static pages" do
+    		it { should have_selector('h1',    text: heading) }
+    		it { should have_selector('title', text: full_title(page_title)) }
+  	end
 
-  describe "Home page" do
-    before { visit root_path }
-    let(:heading)    { 'Sample App' }
-    let(:page_title) { '' }
+	describe "Home page" do
+	    	before { visit root_path }
+	    	let(:heading)    { 'Sample App' }
+	    	let(:page_title) { '' }
 
-    it_should_behave_like "all static pages"
-    it { should_not have_selector 'title', text: '| Home' }
+	    	it_should_behave_like "all static pages"
+	    	it { should_not have_selector 'title', text: '| Home' }
 
-    describe "for signed-in users" do
-      let(:user) { FactoryGirl.create(:user) }
-      before do
-        FactoryGirl.create(:micropost, user: user, content: "Lorem ipsum")
-        FactoryGirl.create(:micropost, user: user, content: "Dolor sit amet")
-        sign_in user
-        visit root_path
-      end
+	    	describe "for signed-in users" do
+	      		let(:user) { FactoryGirl.create(:user) }
+	      		before do
+	        		FactoryGirl.create(:micropost, user: user, content: "Lorem ipsum")
+	        		FactoryGirl.create(:micropost, user: user, content: "Dolor sit amet")
+	        		sign_in user
+	        		visit root_path
+	      		end
 
-      it "should render the user's feed" do
-        user.feed.each do |item|
-          page.should have_selector("li##{item.id}", text: item.content)
-        end
-      end
-    end
-  end
+	      		it "should render the user's feed" do
+	        		user.feed.each do |item|
+	          			page.should have_selector("li##{item.id}", text: item.content)
+	        		end
+	      		end
+	    	
+		    	describe "follower/following counts" do
+	        		let(:other_user) { FactoryGirl.create(:user) }
+	        		before do
+	          			other_user.follow!(user)
+	          			visit root_path
+	        		end
+
+	        		it { should have_link("0 following", href: following_user_path(user)) }
+	        		it { should have_link("1 followers", href: followers_user_path(user)) }
+	      		end
+		end
+	end 
 end
